be6ed7cb0d4a14b193cf4ed3c0dd6dee224c0f37
Tharindu37 <tharindut520@gmail.com>
2024-12-17 00:47:38 +0530

    start

---
+++ src/main/java/com/esell/esell_eureka_server/EsellEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.esell.esell_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EsellEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsellEurekaServerApplication.class, args);
+	}
+
+}
